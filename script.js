5ed6d783e6ae343196f27630fdf95e3347954c5f
Abhirami2005 <m9998641@gmail.com>
2026-06-15 12:48:35 +0530

    Added responsive CSS and dark mode

---
+++ script.js
@@ -0,0 +1,7 @@
+const themeBtn = document.getElementById("theme-toggle");
+
+if(themeBtn){
+    themeBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+}
